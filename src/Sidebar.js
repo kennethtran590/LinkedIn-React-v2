33353a79659dea5f5ccff9d5124112a7cf7d2440
Kenneth Tran <48599140+kennethtran590@users.noreply.github.com>
2022-04-24 23:53:41 -0700

    Implemented Sidebar component with styling

---
+++ src/Sidebar.js
@@ -0,0 +1,46 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img
+          src="https://geekhack.org/index.php?action=dlattach;topic=102350.0;attach=234805;image"
+          alt=""
+        />
+        <Avatar className="sidebar__avatar" />
+        <h2>Kenneth Tran</h2>
+        <h4>kennethtran@gmail.com</h4>
+      </div>
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you </p>
+          <p className="sidebar__statNumber">2,352</p>
+        </div>
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">2,352</p>
+        </div>
+      </div>
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("softwareengineering")}
+        {recentItem("design")}
+        {recentItem("developer")}
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
