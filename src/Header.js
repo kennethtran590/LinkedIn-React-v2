d7d9fdf63bab50d4a775ff0494c815d87d98476e
Kenneth Tran <48599140+kennethtran590@users.noreply.github.com>
2022-04-24 23:52:57 -0700

    Added Header component with styling

---
+++ src/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import HeaderOption from "./HeaderOption";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://cdn-icons.flaticon.com/png/512/3536/premium/3536505.png?token=exp=1650861895~hmac=af40334f59da8858bbbf03b29794a404"
+          alt=""
+        />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://media-exp1.licdn.com/dms/image/C5603AQHQPkLDPuH9uQ/profile-displayphoto-shrink_800_800/0/1541400184198?e=1656547200&v=beta&t=mZiNMvarIpmyfgsbjK_h2e38PilgIPnWZtBJMFM8zWQ"
+          title="Me"
+        />
+      </div>
+    </div>
+  );
+}
+export default Header;
